07656924724e43277664f1997eb0e11acb59c7e5
Dariya116 <d.alexandrova116@yandex.ru>
2023-09-05 12:00:03 +0300

    HW5new

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,15 +1,32 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate, Link } from "react-router-dom";
 import styles from "./login.module.scss";
 
+
 function Login () {
+  const navigate = useNavigate();
+  const [loginToLogin, setLoginToLogin] =useState("");
+  const [passwordToLogin, setPasswordToLogin] =useState("");
+
+  const verificationLogin = () => {
+    if (loginToLogin === localStorage.getItem("login") && passwordToLogin === localStorage.getItem("password")) {
+      navigate("/");
+  } else {
+    alert("Проверьте логин и пароль!")
+  }
+}
   return (
     <div className={styles.login}>
       <div className={styles.logo}>
           <img className={styles.logo__image} src="img/logo2.png" alt="logo" />
       </div>
-      <input placeholder="Почта"/>
-      <input placeholder="Пароль"/>
-      <button className={styles.login__button1} type="button">Войти</button>
+      <input type="text"
+        value={loginToLogin}
+        onChange={(e) => setLoginToLogin(e.target.value)} placeholder="Почта"/>
+      <input type="password"
+        value={passwordToLogin}
+        onChange={(e) => setPasswordToLogin(e.target.value)} placeholder="Пароль"/>
+      <button onClick={() => verificationLogin()} className={styles.login__button1} type="button">Войти</button>
       <Link to="/register"><button className={styles.login__button2} type="button">Зарегестрироваться</button></Link>
     </div>
   );
